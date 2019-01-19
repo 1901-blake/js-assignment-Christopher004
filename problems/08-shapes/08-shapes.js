550b2449d8083dc554c1912e817c45959fc21b4e
Christopher <christopher.gonzalez9@upr.edu>
2019-01-19 17:09:35 -0500

    file with changes

--- problems/08-shapes/08-shapes.js
+++ problems/08-shapes/08-shapes.js
@@ -20,6 +20,59 @@ Example for printShape("Diamond", 5, "*");
  ***
   * 
 */
-function printShape(shape, height, character) {
-  
+function PrintShape(shape,height,character){
+  switch(shape){
+      case "Square":
+      let boo = "";
+      for(let i =0; i<height;i++){
+          boo=boo+character;
+      }
+      for(let i =0; i<height;i++){
+          console.log(boo);
+      }
+      break;
+      
+      case "Triangle":
+          let boo1 = "";
+      for(let i =0; i<=height;i++){
+          console.log(boo1);
+          boo1=boo1+character;
+      }
+      break;
+      
+      case "Diamond":
+      let boo2 = "";
+      let spaces = height/2;
+      let spacesstr ="";
+      let chars = 1;
+      let h =height;
+      do{
+      //populate spaces
+      for(let i =0; i< spaces; i++){
+      boo2=boo2+" ";
+      }
+      //populate characters
+      for(let i =1; i<= chars;i++){
+          boo2 = boo2+ character;
+      }
+              console.log(boo2);
+              if(h>height/2+1){
+              boo2="";
+              chars=chars+2;
+              spaces--;
+              h--;
+              }
+              else{
+                  spaces++;
+                  chars=chars-2;
+                  boo2="";
+                  h--;
+              }
+}while(h>=0);
+      break;
+  }
 }
+
+PrintShape("Square",3,"*");
+PrintShape("Triangle",5,"$");
+PrintShape("Diamond",7,"*")
