6aad0b26c0760d3c6b054dc5b1f35a8d8404b9b7
Christopher <christopher.gonzalez9@upr.edu>
2019-01-19 17:10:32 -0500

    file with changes

--- problems/09-object-properties/09-object-properties.js
+++ problems/09-object-properties/09-object-properties.js
@@ -2,5 +2,11 @@
 Define function objectProperties(someObj)
 Print every property and it's value. */
 function objectProperties(someObj) {
-
+    for(const obj in someObj) {
+console.log(`${obj}: ${someObj[obj]} `);
+    }
 }
+
+let motorcycle = {model: 'bmw', material: 'aluminium', tires: "4", transmission: "standard"};
+
+objectProperties(motorcycle);
