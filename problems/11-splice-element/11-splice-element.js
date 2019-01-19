4a6fb67a02a6e2617302c818f6441239b19743ab
Christopher <christopher.gonzalez9@upr.edu>
2019-01-19 17:11:24 -0500

    file with changes

--- problems/11-splice-element/11-splice-element.js
+++ problems/11-splice-element/11-splice-element.js
@@ -3,5 +3,9 @@ Define function spliceElement(someArr, index)
 Splice the element at the provided index in the array.
 */
 function spliceElement(someArr, index) {
-  
+    let someObj = someArr.splice(index, 3); 
+    return someObj;
 }
+let fruits = ['Banana', 'Orarge', 'Apple', 'Mango'];
+
+console.log(spliceElement(fruits));
