f672ddffde0dd67990337210d412e9a336d17543
Christopher <christopher.gonzalez9@upr.edu>
2019-01-19 17:00:34 -0500

    file with changes

--- problems/03-reverse-string/03-reverse-string.js
+++ problems/03-reverse-string/03-reverse-string.js
@@ -1,6 +1,10 @@
 /* 3. Reverse String
 Define function: reverseStr(someStr)
 Reverse and return the String. */
-function reverseStr(someStr) {
-  
-}
+function reverseStr (someStr) {
+    return someStr.split( '' ).reverse( ).join( '' );;
+    }
+    
+   //declaring variables
+    let someStr = "Christopher";
+console.log(reverseStr(someStr));
