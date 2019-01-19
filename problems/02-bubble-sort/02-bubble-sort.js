2bf3264f1cf0bd395a4ba549d6790ac9ca3c7821
Christopher <christopher.gonzalez9@upr.edu>
2019-01-19 16:59:34 -0500

    folder with changes

--- problems/02-bubble-sort/02-bubble-sort.js
+++ problems/02-bubble-sort/02-bubble-sort.js
@@ -2,6 +2,18 @@
 Define function: bubbleSort(numArray)
 Use the bubble sort algorithm to sort the array.
 Return the sorted array. */
+const numArray = [9,8,7,6,5,4,3,2,1];
 function bubbleSort(numArray) {
-  
+  let swapped;
+  do {
+    swapped = false;
+    for(let i = 0; i < numArray.length; i++) {
+      if(numArray[i] && numArray[i + 1] && numArray[i] > numArray[i + 1]) {
+        [numArray[i], numArray[i + 1]] = [numArray[i + 1],numArray[i]];
+        swapped = true;
+      }
+    }
+  } while(swapped);
+  return numArray;
 }
+console.log(bubbleSort(numArray.slice())); 
