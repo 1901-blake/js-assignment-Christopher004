e1de7085aadf1f341623682442a237b0a8bdb80a
Christopher <christopher.gonzalez9@upr.edu>
2019-01-19 17:08:29 -0500

    file with changes

--- problems/07-palindrome/07-palindrome.js
+++ problems/07-palindrome/07-palindrome.js
@@ -1,6 +1,8 @@
 /* 7. Palindrome
 Define function isPalindrome(someStr)
 Return true if someStr is a palindrome, otherwise return false */
-function isPalindrome(someStr) {
-  
-}
+function isPalindrome(n,x) {
+    return(x=x||0)<0||x>=n.length>>1||n[x]==n[n.length-1-x]&&isPalindrome(n,++x);
+   }
+   console.log(isPalindrome('radar'));
+   console.log(isPalindrome('forget'));
